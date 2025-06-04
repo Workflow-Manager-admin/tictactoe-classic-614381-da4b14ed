bc959c1153338c5c039ed337c03580aef4be5363
Prasanth <prasanth@kavia.ai>
2025-06-04 18:01:33 +0530

    Auto-commit: Changes as of 2025-06-04T12:31:33.647282+00:00

--- tic_tac_toe/src/App.js
+++ tic_tac_toe/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import './TicTacToeClassic.css';
+import TicTacToeClassic from './TicTacToeClassic';
 
 function App() {
   return (
@@ -10,24 +12,13 @@ function App() {
             <div className="logo">
               <span className="logo-symbol">*</span> KAVIA AI
             </div>
-            <button className="btn">Template Button</button>
           </div>
         </div>
       </nav>
 
       <main>
-        <div className="container">
-          <div className="hero">
-            <div className="subtitle">AI Workflow Manager Template</div>
-            
-            <h1 className="title">tic_tac_toe</h1>
-            
-            <div className="description">
-              Start building your application.
-            </div>
-            
-            <button className="btn btn-large">Button</button>
-          </div>
+        <div className="container" style={{ paddingTop: 100 }}>
+          <TicTacToeClassic />
         </div>
       </main>
     </div>
